969820b05e85880edb4d69408504868a0e66ef9c
mlydigsen <mlydigsen@umass.edu>
2015-04-07 15:59:01 +0000

    added team notes and made modification to Plannit files

---
+++ Plannit/routes/login.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+//typing in the name of site will bring you to login page
+router.get('/', function(req, res){
+  res.redirect('/login');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Plannit' });
+});
+
+
+//this will display the sign up page for new users
+router.get('/login/newUser', function(req, res){
+  console.log('hello');
+  res.render('addUser',{title: 'NewUser' });  
+});
+
+
+//this will be called when the form has been filled out so that we can 
+//add a new user, this function should redirect to /users/home once the user
+//has been created otherwise send us back to newUser page
+router.get('/addNewUser', function(req, res){
+  //res.redirect('/users/home');
+});
+
+module.exports = router;
--- Plannit/routes/users.js
+++ Plannit/routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+//this function should be called before we render any view to ensure user
+//if logged in
+function auth(user){
+  if (user === undefined || online[user.uid] === undefined) {
+    req.flash('auth', 'Not logged in!');
+    res.redirect('/user/login');
+    return;
+  }
+}
+
+//this ensures authentication and reroutes if needed
+router.post('/auth', function(req, res) {
+  // redirect if logged in:
+  var user = req.session.user;
+
+  // do the check as described in the `exports.login` function.
+  if (user !== undefined && online[user.uid] !== undefined) {
+    res.redirect('/user/main');
+  }
+  else {
+    // Pull the values from the form.
+    var username = req.body.username;
+    var password = req.body.password;
+    // Perform the user lookup.
+    userlib.lookup(username, password, function(error, user) {
+      if (error) {
+        // If there is an error we "flash" a message to the
+        // redirected route `/user/login`.
+        req.flash('auth', error);
+        res.redirect('/user/login');
+      }
+      else {
+        req.session.user = user;
+        // Store the user in our in memory database.
+        online[user.uid] = user;
+        // Redirect to main.
+        res.redirect('/user/main');
+      }
+    });
+  }
+});
+
+//this is the users home where all of the users planners and todo list will
+//be displayed
+router.get('/home', function(req, res, next) {
+  //this is calling our auth function that will check to see if the user 
+  //is online if he isn't the auth funtion will redirect to user login 
+  //with message
+  if(auth(req.session.user)){
+ 
+  }
+  else{
+    res.render;
+  }
 });
 
 module.exports = router;
